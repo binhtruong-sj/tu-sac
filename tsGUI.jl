c5f527d6b7a03e37fca891d52fcbd615c5005909
binhtruong-sj <binhtruong.sj@gmail.com>
2022-09-21 21:22:10 -0700

    Add files via upload

--- tsGUI.jl
+++ tsGUI.jl
@@ -3210,7 +3210,7 @@ function chk2(playCard)
             break
         end
     end
-    for p = 1:3
+    for p = 1:2
         print("   pair-",p," -- ")
 
         for ap in allPairs[p]
@@ -3698,7 +3698,7 @@ end
                         end
                     end
                 end
-            else
+            elseif length(cards) < 3
                 println("trashCnt")
                 TrashCnt = length(chot1s)
                 thand = deepcopy(hand)
