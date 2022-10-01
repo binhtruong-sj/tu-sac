8f940a14b85ce5b486b8abff63c035fdbd081c22
binhtruong-sj <binhtruong.sj@gmail.com>
2022-09-30 23:28:17 -0700

    Add files via upload

--- runTUSAC_m_client2.jl
+++ runTUSAC_m_client2.jl
@@ -1,4 +1,11 @@
 using GameZero
-
-rungame("tsGUI.jl")
+wf = open("tempSGUI.jl","w")
+rf = open("tsGUI.jl","r")
+while !eof(rf)
+    aline = readline(rf)
+    println(wf,aline)
+end
+close(rf)
+close(wf)
+rungame("tempSGUI.jl")
 
